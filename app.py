ca2171b4f8e73756ef6760bbb1cb56cbcdc5dcdf
Dimitri GAILLOT <dimitri.gaillot@gmail.com>
2021-08-20 14:46:07 +0200

    adding map and pandas requiremeent

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 '''
-# TaxiFareModel front
+# Taxi fare price predictor
 '''
+
+st.markdown('''
+### **"It's something i guess"** 
+### - *random people*
+
+_______________
+''')
 
 
 date = st.date_input("date and time")
@@ -30,47 +38,37 @@ response = requests.get(url_api, params=params).json()
 
 prediction = response['prediction']
 
-st.write(f'Your fare will be ${int(prediction)}')
+st.markdown(f'''
+_______________
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+## Your fare will be ${int(prediction)}
+_______________
 ''')
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
+if st.checkbox('Show map', False):
+    df = pd.DataFrame(
+            [
+                [pickup_latitude, pickup_longitude],
+                [dropoff_latitude, dropoff_longitude]
+            ],
+            columns = ['lat', 'lon']
+        )
+    st.map(df)
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
-url = 'https://taxifare.lewagon.ai/predict'
+# CSS = """
+# h1 {
+#     color: black;
+# }
+# .stApp {
+#     background-color: #ba93b6;
+#     background-size: cover;
+# }
+# """
 
-if url == 'https://taxifare.lewagon.ai/predict':
+# st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
